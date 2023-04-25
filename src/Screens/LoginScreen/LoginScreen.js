8f12e9d154ed5ffec8389490ec5d686c920cfa1a
Anton <a.chertok@akatech.com.ua>
2023-04-25 14:11:35 +0300

    delete Screens

--- src/Screens/LoginScreen/LoginScreen.js
+++
@@ -1,6 +0,0 @@
-import AuthView from "../../components/AuthView";
-import { Login } from "../../components/Login";
-
-export const LoginScreen = () => {
-  return <AuthView WrappedComponent={Login} />;
-};
--- src/Screens/RegistrationScreen/RegistrationScreen.js
+++
@@ -1,6 +0,0 @@
-import AuthView from "../../components/AuthView";
-import { Register } from "../../components/Register";
-
-export const RegistrationScreen = () => {
-  return <AuthView WrappedComponent={Register} />;
-};
